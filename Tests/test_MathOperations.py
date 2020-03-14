aea394475cc083f03224acf2ba8e51db8b9a639d
kamal <ky83@njit.edu>
2020-03-13 23:15:36 -0400

    Task: Added Multiplication with tests

--- Tests/test_MathOperations.py
+++ Tests/test_MathOperations.py
@@ -15,10 +15,18 @@ class MyTestCase(unittest.TestCase):
         result = self.calculator.Sum(1, 2)
         self.assertEqual(3, result)
 
+    def test_calculator_return_sumList(self):
+        numlist = [1, 3, 5, 2]
+        result = self.calculator.Sum(numlist)
+        self.assertEqual(11, result)
+
     def test_calculator_return_difference(self):
         result = self.calculator.Difference(4, 1)
         self.assertEqual(3, result)
 
-    def test_calculator_return_division(self):
-        result = self.calculator.Division(15,5)
-        self.assertEqual(3,result)
+    def test_MathOperation_Product(self):
+        self.assertEqual(10, self.calculator.Product(2, 5))
+
+    def test_MathOperations_Product_list(self):
+        numlist = [1, 3, 5]
+        self.assertEqual(15, self.calculator.Product(numlist))
